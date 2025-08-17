bef8476fd4aec276c11fc21982f4cf8f07cf91c1
Claudio Cuellar <ccuellar@wine.com>
2025-08-17 09:56:38 -0400

    Adding default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: '/auth',
+        pathMatch: 'full'
+    },
+    {
         path: 'auth', loadChildren: () => import('./features/auth/auth.routes').then((m) => m.authRoutes),
     },
 ];
