7e04c79e58d132cc948df8692e6a1861f95063f3
Claudio Cuellar <ccuellar@wine.com>
2025-08-17 10:00:19 -0400

    Fixed route

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 
 export const authRoutes: Routes = [
-  { path: '/', component: LoginComponent },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
   { path: 'login', component: LoginComponent },
 ];
