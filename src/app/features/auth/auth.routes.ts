9753a24538bcc9f574b1750d726271f8831dde05
Claudio Cuellar <ccuellar@wine.com>
2025-08-17 09:57:32 -0400

    Adding default route to auth

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 
 export const authRoutes: Routes = [
+  { path: '/', component: LoginComponent },
   { path: 'login', component: LoginComponent },
 ];
