bc7b2f8a36141a41305c3829cd2510e7dec28b79
Claudio Cuellar <ccuellar@wine.com>
2025-09-15 07:05:43 -0400

    Adding logo

--- src/app/core/services/menu/menu.service.ts
+++ src/app/core/services/menu/menu.service.ts
@@ -44,17 +44,17 @@ export class MenuService {
           `),
           route: '/main/categories',
         },
-        {
-          id: 'reports',
-          label: 'Reports',
-          icon: this.sanitizer.bypassSecurityTrustHtml(`
-            <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-              <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-            </svg>
-          `),
-          route: '/main/reports',
-        },
+        // {
+        //   id: 'reports',
+        //   label: 'Reports',
+        //   icon: this.sanitizer.bypassSecurityTrustHtml(`
+        //     <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        //       <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+        //       <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+        //     </svg>
+        //   `),
+        //   route: '/main/reports',
+        // },
         // {
         //   id: 'pages',
         //   label: 'Pages',
